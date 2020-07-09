dc54896b9f578f424301f9d119ebf00fdd66328f
JBrozinski <joseph.brozinski@gmail.com>
2020-07-08 23:35:25 -0600

    added new images to public assets folder

---
+++ build/precache-manifest.63c199cf3e04ec9f533b684fd2e3c679.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb74bfa0ff62e5fbaeb1",
+    "url": "/static/css/main.7feb9300.chunk.css"
+  },
+  {
+    "revision": "eb74bfa0ff62e5fbaeb1",
+    "url": "/static/js/main.6f5b52e0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f2fde74afd5c17a7c10a",
+    "url": "/static/js/2.84779473.chunk.js"
+  },
+  {
+    "revision": "5815a124c5755ab3077a9ac036271082",
+    "url": "/index.html"
+  }
+];
